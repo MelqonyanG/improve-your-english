cbf62107e4cf0488b02657ed7e7aa86747e344de
MelqonyanG <melqonyangayane94@gmail.com>
2019-10-13 03:18:01 +0400

    add words

--- src/Content.js
+++ src/Content.js
@@ -2,28 +2,13 @@ import React from 'react';
 import Show from './Show';
 import Practice from './Practice';
 
-function readWords(level){
-    var request = new XMLHttpRequest();
-    request.open('GET', `https://melqonyang.github.io/improve-your-english/words/${level}.txt`, true);
-    request.send(null);
-    request.onreadystatechange = () => {
-        if (request.readyState === 4 && request.status === 200) {
-            var type = request.getResponseHeader('Content-Type');
-            if (type.indexOf("text") !== 1) {
-                console.log(request.responseText.split('\n'));
-            }
-        }
-    }
-    return ["apple * խնձոր", "name * անուն", "father * հայր"]
-}
-
 function orderWords(words, order){
     if (order === 'alphabetical'){
         words.sort(function (a, b) {
-            if (a[0] > b[0]) {
+            if (a[0].toLowerCase() > b[0].toLowerCase()) {
                 return 1;
             }
-            if (b[0] > a[0]) {
+            if (b[0].toLowerCase() > a[0].toLowerCase()) {
                 return -1;
             }
             return 0;
@@ -44,13 +29,15 @@ function orderWords(words, order){
 
 function fixDirection(words, direction){
     const result = [];
-    for (let i=0; i<words.length; i+=1){
-        let arm = words[i].split("*")[1].trim();
-        let eng = words[i].split("*")[0].trim();
-        if(direction){
-            result.push([arm, eng]);
-        }else{
-            result.push([eng, arm])
+    for (let i=0; i<words.length; i+=1){       
+        if(words[i].trim().length > 0){
+            let arm = words[i].split("*")[1].trim();
+            let eng = words[i].split("*")[0].trim();
+            if(direction){
+                result.push([arm, eng]);
+            }else{
+                result.push([eng, arm])
+            }
         }
     }
     return result;
@@ -58,17 +45,47 @@ function fixDirection(words, direction){
 
 export default class Content extends React.Component{
     constructor(props){
-        super(props);
+        super(props);        
         this.state = {
-            all: readWords(props.level),
+            all: [],
             wrong: [],
             correct: [],
             update: true
         }
     }
 
-    componentWillReceiveProps(){
+    componentDidMount(){
+        const {level} = this.props;
+        var request = new XMLHttpRequest();
+        request.open('GET', `https://melqonyang.github.io/spelling-checker/words/${level}.txt`, true);
+        request.send(null);
+        request.onreadystatechange = () => {
+            if (request.readyState === 4 && request.status === 200) {
+                var type = request.getResponseHeader('Content-Type');
+                if (type.indexOf("text") !== 1) {
+                    this.setState({all: request.responseText.split('\n')});
+                }
+            }
+        }
+    }
+
+    componentWillReceiveProps(nextProps){
         this.setState({update: true});
+        if(this.props.level !== nextProps.level){
+            const {level} = nextProps;
+            var request = new XMLHttpRequest();
+            request.open('GET', `https://melqonyang.github.io/spelling-checker/words/${level}.txt`, true);
+            request.send(null);
+            request.onreadystatechange = () => {
+                if (request.readyState === 4 && request.status === 200) {
+                    var type = request.getResponseHeader('Content-Type');
+                    if (type.indexOf("text") !== 1) {
+                        this.setState({all: request.responseText.split('\n')});
+                    }
+                }
+            } 
+        }
+        
     }
 
     shouldComponentUpdate(nextProps, nextState) {            
@@ -106,7 +123,7 @@ export default class Content extends React.Component{
 
     render(){
         const {mode, direction, count, order} = this.props;
-        let words = this.state[count];
+        let words = this.state[count];       
         const {correct, wrong} = this.state;
         words = fixDirection(words, direction);       
         words = orderWords([...words], order);
